ca484c848ad52b7ea94c0fe8955ec9cfd1db15da
hkwuks <25031515+hkwuks@users.noreply.github.com>
2025-07-02 23:29:08 +0800

    Update adaptive_rag.py

--- all_rag/adaptive_rag.py
+++ all_rag/adaptive_rag.py
@@ -31,7 +31,20 @@ class QueryClassifier:
             input_variables=['query'],
             template='Classify the following query into one of these categories: Factual, Analytical, Opinion, or Contextual.\nQuery:{query}\nCategory:'
         )
-        self.chain = self.prompt|self.llm.with_structured_output(categories_options)
+        self.chain = self.prompt | self.llm.with_structured_output(categories_options)
 
     def classify(self, query):
         return self.chain.invoke(query).category
+
+
+class BaseRetrievalStrategy:
+    def __init__(self, texts):
+        self.embeddings = OpenAIEmbeddings()
+        text_splitter = CharacterTextSplitter(chunk_size=800, chunk_overlap=0)
+        self.documents = text_splitter.create_documents(texts)
+        self.db = FAISS.from_documents(self.documents, self.embeddings)
+        self.llm = ChatOpenAI(temperature=0,model='gpt-4o',max_tokens=4000)
+
+    def retrieve(self,query,k=4):
+        return self.db.similarity_serch(query,k=k)
+    
